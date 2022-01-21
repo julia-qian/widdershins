def23973e3e3f0f050e13861c9046326e0ae73e2
Julia Qian <jqian@Julias-MacBook-Pro.local>
2022-01-21 12:16:19 -0500

    refactored to use only 1 file to generate markdown

--- autogen.py
+++ autogen.py
@@ -2,34 +2,36 @@ import os
 import webbrowser
 
 # make sure you are connected to VPN
+# uplift-python venv (to run events_openapi.py)
 # must be in widdershins folder
+ 
+
+os.chdir('../uplift-python')
+os.system("python3 events_openapi.py")
+os.chdir("../widdershins")
 
 api_urls = [
+    'https://lpbs.prodgw.uplift-platform.com/info?openapi_spec=json',
     'https://ls.dev01.uplift-platform.com/info?openapi_spec=json',
-    'https://ems.dev01.uplift-platform.com/openapi.json'
+    'https://ems.dev01.uplift-platform.com/openapi.json',
+    '../uplift-python/event_api.json'
 ]
 
 
 print(f"There are currently {len(api_urls)} openAPI docs")
 
-for index, url in enumerate(api_urls): 
-    #clear files first?   
-    os.system(f"node widdershins {url} markdown{index}.md")
-
-print("Generating final Markdown file")
-
 with open('../slate/source/index.html.md', 'w') as outfile:
-    for index, _ in enumerate(api_urls):
-        with open(f"markdown{index}.md") as infile:
+    for index, url in enumerate(api_urls): 
+        os.system(f"node widdershins {url} temp.md")
+        with open("temp.md") as infile:
             if index != 0:
-                for i, line in enumerate(infile):
-                    if i not in range(0, 20): 
-                        outfile.write(line)
+                    for i, line in enumerate(infile):
+                        if i not in range(0, 20): 
+                            outfile.write(line)
             else:
                 for line in infile:
                     outfile.write(line)
-            
-        
+     
 
 print("Successful conversion to Markdown, saved as ../slate/source/index.html.md")
 print("Opening slate")
@@ -37,13 +39,4 @@ print("Opening slate")
 os.chdir('../slate')
 os.system("pwd")
 os.system("bundle exec middleman server")
-
-webbrowser.open("http://localhost:4567", new=0, autoraise=True)
-
-
-#for each link
-# api -> markdown in widdershins
-# save markdown in global file
-
-#after, load slate
 
